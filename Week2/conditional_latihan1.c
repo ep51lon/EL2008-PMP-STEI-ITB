3f0a5e18ee0908bcff805d24d89a9b50dc994824
tnunay <40408057+tnunay@users.noreply.github.com>
2025-02-26 21:22:43 +0700

    Update default solution for latihan1

--- Week2/conditional_latihan1.c
+++ Week2/conditional_latihan1.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
-    float prob_hujan, jarak; // Variabel untuk menyimpan probabilitas hujan
+    float prob_hujan; // Variabel untuk menyimpan probabilitas hujan
 
     // Meminta pengguna memasukkan probabilitas hujan
     printf("Masukkan probabilitas hujan hari ini (0-100%%): ");
     scanf("%f", &prob_hujan);
-
-    printf("Jarak: ");
-    scanf("%f", &jarak);
-
-    printf("Jarak dari rumah ke kampus: %.2f, probabilitas hujan hari ini: %.1f\n", jarak, prob_hujan);
 
     // Menentukan moda transportasi berdasarkan probabilitas hujan
     if (prob_hujan > 60) {
